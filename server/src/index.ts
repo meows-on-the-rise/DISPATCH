6c30ab6cb7b18dd000ac3ac0aeca77ef88d6a9e0
meows-on-the-rise <rakoloib@gmail.com>
2026-04-18 19:44:45 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,11 +20,10 @@ const httpServer = http.createServer(app);
 
 app.set("trust proxy", 1);
 app.use(helmet());
-app.use(
-  cors({
-    origin: "*",
-    credentials: false,
-  })
+app.use(cors({
+  origin: "https://dispatch-two-lac.vercel.app",
+  credentials: true
+})
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
