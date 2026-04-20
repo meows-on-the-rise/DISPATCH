4a0e6050517d57ff7e1997e7c46ca610fa525d06
meows-on-the-rise <rakoloib@gmail.com>
2026-04-21 00:18:10 +0200

    Update AdminVerifyPage.tsx

--- src/pages/admin/AdminVerifyPage.tsx
+++ src/pages/admin/AdminVerifyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useToast } from "../../lib/toast";
 import { Icons } from "../../components/shared";
